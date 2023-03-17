4c5b0d42d0a1abc346b49099899d2af5c6b3d685
exiasun <exiasun@tencent.com>
2023-03-17 23:20:54 +0800

    调整部分参数，看看能不能通过2C的最后两个测试

--- src/raft/raftconf.go
+++ src/raft/raftconf.go
@@ -12,15 +12,15 @@ func getRandFollowerHeartBeatCheckDuration() time.Duration {
 
 //getFollowerHeartBeatExpireTime 如果多长时间没有收到来自leader的心跳，则判断失败
 func getFollowerHeartBeatExpireTime() time.Duration {
-	return time.Duration(500) * time.Millisecond
+	return time.Duration(400) * time.Millisecond
 }
 
 //getLeaderHeartBeatDuration 作为follower，多长时间检查一次心跳有没有过期
 func getLeaderHeartBeatDuration() time.Duration {
-	return time.Duration(100) * time.Millisecond
+	return time.Duration(70) * time.Millisecond
 }
 
-//getRandCandidateOverTimeTickDuration 作为follower，多长时间检查一次心跳有没有过期
+//getRandCandidateOverTimeTickDuration 作为candidate，多长时间选举过期，进行下一轮选举
 func getRandCandidateOverTimeTickDuration() time.Duration {
-	return time.Duration(200+rand.Int()%200) * time.Millisecond
+	return time.Duration(300+rand.Int()%200) * time.Millisecond
 }
