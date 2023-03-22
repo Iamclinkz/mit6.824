71b096bae6b8598822aeb65d1edbd371bd19fbd8
exiasun <exiasun@tencent.com>
2023-03-22 12:59:54 +0800

    logs

--- src/raft/leader.go
+++ src/raft/leader.go
@@ -48,13 +48,18 @@ func (rf LeaderStateHandler) OnAppendEntriesReply(msg *AppendEntriesReplyMsg) {
 
 	if msg.reply.Success {
 		//如果成功，那么设置matchIndex和nextIndex
-
-		nextIndexFromReply := msg.args.PrevLogIndex + len(msg.args.Entries) + 1
-		nextMatchFromReply := nextIndexFromReply - 1
+		nextMatchFromReply := msg.args.PrevLogIndex + len(msg.args.Entries)
+		nextIndexFromReply := nextMatchFromReply + 1
 
 		//这里rpc可能乱序，所以需要保证一手小的不能覆盖大的
 		//如果msg中的条目都匹配成功了，但是以前就匹配过了，那么可能是rpc乱序了，也可能是本次没有增加新的条目，总之我们不做处理
-		if nextIndexFromReply <= rf.nextIndex[peerID] || nextMatchFromReply <= rf.matchIndex[peerID] {
+		if nextIndexFromReply <= rf.nextIndex[peerID] {
+			rf.log(dWarn, "S%v try to reduce nextIndex: %v -> %v", peerID, rf.nextIndex[peerID], nextIndexFromReply)
+			return
+		}
+
+		if nextMatchFromReply <= rf.matchIndex[peerID] {
+			rf.log(dWarn, "S%v try to reduce matchIndex: %v -> %v", peerID, rf.matchIndex[peerID], nextMatchFromReply)
 			return
 		}
 
